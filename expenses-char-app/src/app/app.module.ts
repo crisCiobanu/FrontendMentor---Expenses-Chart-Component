bd898820118e96907e0e32761bb458ce3f6aa446
crisCiobanu <ciobanucristian.cc@gmail.com>
2023-02-27 13:44:34 +0100

    Chart component added

--- expenses-char-app/src/app/app.module.ts
+++ expenses-char-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpenseChartComponent } from './expense-chart/expense-chart.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpenseChartComponent
+    ExpenseChartComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
